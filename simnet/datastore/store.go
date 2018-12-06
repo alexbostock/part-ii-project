7acfef0a88e85c983a4aa1f765d672d3db666a7a
alexbostock <alex@alexbostock.co.uk>
2018-12-06 11:07:28 +0000

    Refactor dbnode to accomodate quorum assembly

--- simnet/datastore/store.go
+++ simnet/datastore/store.go
@@ -4,14 +4,14 @@ import (
 	"os"
 )
 
-type datastore interface {
+type Store interface {
 	Get(key []byte) []byte          // Returns a value, or nil to indicate failure
 	Put(key []byte, val []byte) int // Returns a unique non-zero id if write was successful (requires a commit call to complete)
 	Commit(key []byte, id int) bool // Returns true iff the transaction with id id was successfully committed
 	DeleteStore()                   // Delete the store (including removing all data from disk)
 }
 
-func CreateStore(path string) datastore {
+func CreateStore(path string) Store {
 	if path == "" {
 		// TODO: return an in-memory store in this case
 		return nil
--- simnet/dbnode.go
+++ simnet/dbnode.go
@@ -2,36 +2,72 @@ package simnet
 
 import (
 	"log"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"github.com/alexbostock/part-ii-project/simnet/datastore"
 )
 
-func startNode(id int, incoming chan message, outgoing chan message) {
+type nodestate struct {
+	id             int          // A unique node identifier
+	incoming       chan message // Streams of network messages
+	outgoing       chan message
+	clientReqQueue chan message // Holds client request to avoid blocking incoming
+	mutex          *sync.Mutex
+	store          datastore.Store
+}
+
+func startNode(id int, incoming, outgoing chan message) {
+	state := nodestate{
+		id,
+		incoming,
+		outgoing,
+		make(chan message, 25),
+		&sync.Mutex{},
+		datastore.CreateStore(filepath.Join("data", strconv.Itoa(id))),
+	}
+
+	go handleRequests(state)
+
 	for msg := range incoming {
 		if msg.dest != id {
 			log.Print("Misdelivered message for %d delivered to %d", msg.dest, id)
 		} else {
 			switch msg.demuxKey {
 			case clientReadRequest:
-				outgoing <- message{
-					msg.id,
-					id,
-					msg.src,
-					clientReadResponse,
-					msg.key,
-					make([]byte, 0),
-					false,
-				}
+				state.clientReqQueue <- msg
 			case clientWriteRequest:
-				outgoing <- message{
-					msg.id,
-					id,
-					msg.src,
-					clientWriteResponse,
-					msg.key,
-					msg.value,
-					false,
-				}
+				state.clientReqQueue <- msg
 			default:
 				log.Print("Unexpected message type received %+v", msg)
+			}
+		}
+	}
+}
+
+func handleRequests(node nodestate) {
+	for msg := range node.clientReqQueue {
+		switch msg.demuxKey {
+		case clientReadRequest:
+			node.outgoing <- message{
+				msg.id,
+				node.id,
+				msg.src,
+				clientReadResponse,
+				msg.key,
+				make([]byte, 0),
+				false,
+			}
+		case clientWriteRequest:
+			node.outgoing <- message{
+				msg.id,
+				node.id,
+				msg.src,
+				clientWriteResponse,
+				msg.key,
+				msg.value,
+				false,
 			}
 		}
 	}
