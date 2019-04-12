d9f8370dde2bacda47911edb9ee569b91aecfe08
alexbostock <alex@alexbostock.co.uk>
2019-04-12 11:35:11 +0100

    Add dummy elector to test as if without elections

---
+++ dbnode/elector/dummy.go
@@ -0,0 +1,29 @@
+package elector
+
+import "github.com/alexbostock/part-ii-project/packet"
+
+// A Dummy is a do-nothing elector, equivalent to no election algorithm
+type Dummy struct {
+	id       int
+	outgoing chan packet.Message
+}
+
+func newDummy(id int, outgoing chan packet.Message) *Dummy {
+	return &Dummy{
+		id:       id,
+		outgoing: outgoing,
+	}
+}
+
+func (d *Dummy) Leader() int {
+	return d.id
+}
+
+func (d *Dummy) ForwardToLeader(msg packet.Message) {
+	msg.Dest = d.id
+	d.outgoing <- msg
+}
+
+func (d *Dummy) ProcessMsg(msg packet.Message) {
+	// Do nothing
+}
